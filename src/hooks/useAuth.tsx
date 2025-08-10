041a58837736a4ce8ad85508b16c0d6543c75681
Rizal Nurfari <rizalnurfajri555@gmail.com>
2025-08-10 20:43:49 +0700

    Update

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -73,7 +73,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }
 
   const signUp = async (email: string, password: string) => {
-    const redirectUrl = `${window.location.origin}/login`
+    const redirectUrl = `${window.location.origin}/`
     
     const { error } = await supabase.auth.signUp({
       email,
