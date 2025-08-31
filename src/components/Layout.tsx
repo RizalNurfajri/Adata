e57dfd7fba026edd26917835af7e0a75f4d434da
Rizal Nurfajri <rizalnurfajri555@gmail.com>
2025-08-31 16:21:52 +0700

    Up

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -176,16 +176,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center space-x-2">
-            <img
-              src="/logo-light.webp"
-              alt="Adata"
-              className="h-8 block dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Adata"
-              className="h-8 hidden dark:block"
-            />
+              <img src="/logo.webp" alt="Adata" className="h-6 w-6" />
+              {/* Logo Adata yang menyesuaikan tema */}
+              <img 
+                src="/logo-light.webp" 
+                alt="Adata" 
+                className="h-8 block dark:hidden" 
+              />
+              <img 
+                src="/logo-dark.webp" 
+                alt="Adata" 
+                className="h-8 hidden dark:block" 
+              />
             </Link>
 
             {/* Right side items */}
